9c352e6c3e06804c9f5ae51803849410cefa29c1
Doug Storz <douglasstorz@gmail.com>
2024-01-27 17:31:45 -0500

    Copies image from clipboard

--- jp_image_to_dict/lib/main.dart
+++ jp_image_to_dict/lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:pasteboard/pasteboard.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,22 +16,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            ParseImageSection(),
-            Divider(thickness: 3.0, color: Theme.of(context).dividerColor),
-            Expanded(
-              child: Placeholder(
-                child: Text("Embedded Web View goes Here"),
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        home: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              ParseImageSection(),
+              ImagePreview(),
+              Divider(thickness: 3.0, color: Theme.of(context).dividerColor),
+              Expanded(
+                child: InAppWebView(
+                  initialUrlRequest: URLRequest(
+                      url: WebUri("https://jisho.hlorenzi.com/search/test"),
+                      method: "GET"),
+                ),
+                /*child: Placeholder(
+                  child: Text("Web View Here"),
+                ),
+                */
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class AppState extends ChangeNotifier {
+  String? capturedText;
+  Uint8List? clipImage;
+  double? imageHeight;
+
+  void captureFromClipboard() async {
+    /* Capture Text only
+    ClipboardData? clipboard = await Clipboard.getData('text/plain');
+    print(clipboard?.text);
+    capturedText = clipboard?.text;
+    */
+
+    clipImage = await Pasteboard.image;
+    if (clipImage?.isNotEmpty ?? false) {
+      var decodedImage = await decodeImageFromList(clipImage!);
+      imageHeight = decodedImage.height.toDouble();
+    }
+
+    // TODO: Connect with Cloud Vision and send the image off to them
+    capturedText = "<Not Yet Implemented>";
+
+    notifyListeners();
   }
 }
 
@@ -35,14 +77,26 @@ class ParseImageSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Row(
         children: [
           FilledButton.tonal(
             child: Text("Read Text From Clipboard Image"),
-            onPressed: () {
-              print("Button pressed");
+            onPressed: () async {
+              //print("Button pressed");
+              /* Only gets text
+              ClipboardData? clipboard = await Clipboard.getData('text/plain');
+              print(clipboard?.text);
+              */
+
+              appState.captureFromClipboard();
+
+              //final imageBytes = await Pasteboard.image;
+              //print(imageBytes?.length);
+              //print(imageBytes?.toString());
             },
           ),
           Expanded(
@@ -56,7 +110,9 @@ class ParseImageSection extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
                 child: SizedBox(
-                  child: Center(child: Text("Parsed Text Goes Here")),
+                  child: Center(
+                      child: Text(
+                          appState.capturedText ?? "Parsed Text Goes Here")),
                 ),
               ),
             ),
@@ -64,5 +120,31 @@ class ParseImageSection extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class ImagePreview extends StatelessWidget {
+  const ImagePreview({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+
+    Widget output;
+    double height = 20.0;
+
+    if (appState.clipImage?.isNotEmpty ?? false) {
+      // ! promotes the value to non-nullable since we just checked in the if
+      print("image found and ready to convert from bytes");
+
+      output = Image.memory(appState.clipImage!);
+      height = appState.imageHeight!;
+    } else {
+      output = Placeholder();
+    }
+
+    return SizedBox(height: height, child: output);
   }
 }
--- jp_image_to_dict/macos/Flutter/GeneratedPluginRegistrant.swift
+++ jp_image_to_dict/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_inappwebview_macos
+import pasteboard
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
 }
