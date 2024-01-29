9a8df49c0b47d71674d46fa64b24d9c07625ad52
Doug Storz <douglasstorz@gmail.com>
2024-01-29 12:37:25 -0500

    Comment adjustments

--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,9 @@ app = FastAPI()
 # BAD: Cannot use wildcards in these origins, use allow_origin_regex instead. (Note the lack of pluriality)
 # origins = ["http://localhost:*", "https://localhost:*"]
 
-# DEBUGGING
-# Can use both together. The regex is failing me (even though direct testing shows it works)
-# so why not try both
+# Can use both together
+# The regex failed even though I checked the CORSMiddleWare source and did the re.compile and fullmatch the same way and it worked
+# But adding the explicit domain worked
 origins = ["https://goluxas.github.io"]
 origin_regex = r"https?:\/\/(localhost|goluxas\.github\.io)(:\d{1,5})?"
 
@@ -31,19 +31,21 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "API is online.\nVisit /docs for usage."}
 
 
 # want an endpoint to accept png bytes (however that transfers over API)
 # and sends back all the information flutter will need
 # What is that information?
 # For now, just the parsed text.
-# Later, this backend should do the analysis and dictionary lookup and send that as an object too.
-# Also need a way to communicate errors to Flutter
+# TODO: this backend should do the analysis and dictionary lookup and send that as an object too.
 @app.post("/ocr/png/")
 async def ocr_png(file: UploadFile):
-    # NOTE: MIGHT want to switch to UploadFile, which spools to a file on disk if memory gets too full
-    # Safer for high volume requests. Not necessary for my personal use.
+    """
+    Sends the file off to the OCR engine and processes the response for frontend use.
+    For the time being, that means only the full captured text as a string.
+    """
+
     try:
         # UploadFile.file is the actual file-like object
         # OPTIMIZATION: If the UploadFile is already png then this unnecessarily un and recompresses it.
