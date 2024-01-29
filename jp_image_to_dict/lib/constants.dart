40f142bfbc70ef36c0e0f688358abdae9f15bcdb
Doug Storz <douglasstorz@gmail.com>
2024-01-29 14:54:15 -0500

    Environment variable IS_DEVELOPMENT (default true) chooses API address

--- jp_image_to_dict/lib/constants.dart
+++ jp_image_to_dict/lib/constants.dart
@@ -1,10 +1,17 @@
-class ApiConstants {
-  // My PC static String baseUrl = "minecraft.goluxas.com:8221";
-  // Render Host
-  static String baseUrl = "jp-image-to-dict.onrender.com";
-  static String ocrEndpoint = "/ocr/png/";
-}
-
 class Constants {
   static String lorenziJishoSearchPath = "https://jisho.hlorenzi.com/search/";
+
+  // Defaults to true for safety
+  static const bool isDevelopment =
+      String.fromEnvironment("IS_DEVELOPMENT", defaultValue: "true") == "true";
+}
+
+class ApiConstants {
+  // My PC
+  static const String _localApi = "localhost:8221";
+  // Backend hosted on Render
+  static const String _remoteApi = "jp-image-to-dict.onrender.com";
+
+  static String baseUrl = Constants.isDevelopment ? _localApi : _remoteApi;
+  static String ocrEndpoint = "/ocr/png/";
 }
